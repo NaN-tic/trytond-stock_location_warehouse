2c1933777b92941eb01625d8a0742b3f7aa91105
Guillem Barba <guillem@nan-tic.com>
2013-09-25 11:00:34 +0200

    Fix copyright header of Python and XML files

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,3 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
-
+#The COPYRIGHT file at the top level of this repository contains the full
+#copyright notices and license terms.
 from .test_stock_location_warehouse import suite
