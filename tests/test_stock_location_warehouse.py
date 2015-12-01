ec5f7ec291160997fd7d2bcb890b545700130908
resteve <resteve@zikzakmedia.com>
2015-12-01 09:44:27 +0100

    Tests

--- tests/test_stock_location_warehouse.py
+++ tests/test_stock_location_warehouse.py
@@ -1,27 +1,18 @@
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
+# This file is part of the stock_location_warehouse module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockLocationWarehouseTestCase(unittest.TestCase):
-    'Test stock_location_warehouse module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_location_warehouse')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_location_warehouse')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class StockLocationWarehouseTestCase(ModuleTestCase):
+    'Test Stock Location Warehouse module'
+    module = 'stock_location_warehouse'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            StockLocationWarehouseTestCase))
+        StockLocationWarehouseTestCase))
     return suite
